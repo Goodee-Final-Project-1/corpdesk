ea0156c847d63111ba493323e311433f5be5c4aa
SJ <ligerssj@naver.com>
2025-09-11 11:31:22 +0900

    Init

---
+++ corpdesk/src/main/java/com/goodee/corpdesk/CorpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.goodee.corpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpdeskApplication.class, args);
+	}
+
+}
