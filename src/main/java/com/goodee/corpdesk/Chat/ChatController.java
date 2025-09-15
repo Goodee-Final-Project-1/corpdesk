231f07c91ef5659031f12a2646fa0af0cae936f5
huni99 <wjdrlfgns1@naver.com>
2025-09-15 16:43:51 +0900

    웹 소켓 채팅 컨트롤러 서비스 구현중
    #29

--- src/main/java/com/goodee/corpdesk/Chat/ChatController.java
+++ src/main/java/com/goodee/corpdesk/Chat/ChatController.java
@@ -2,24 +2,36 @@ package com.goodee.corpdesk.Chat;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
+//http요청에 대한 url 매핑
 @RequestMapping("chat/")
 public class ChatController {
-
 	
-	public void sendMessage(ChatMessage msg , Principal principal) {
+	@Autowired
+	//Spring에서 서버가 클라이언트(STOMP 구독자)에게 메시지를 푸시하기 위해 제공하는 템플릿 객체
+	private SimpMessagingTemplate messagingTemplate; 
+	//websocket 요청에 대한 매핑 위의 requestMapping과 관련없고 websocket config에서 지정해준 prefix 사용
+	@MessageMapping("/chat/message")
+	public void chatMessage(ChatMessage msg , Principal principal) {
 		
+		
+		ChatRoom chatRoom = new ChatRoom();
+		messagingTemplate.convertAndSend("/sub/chat/room/"+)
 		 
 	}
 
 @GetMapping("form")
 public String chatForm() {
 	return "Chat/chat_page";
-	
 }
 	
 }
--- src/main/webapp/WEB-INF/views/Chat/chat_page.jsp
+++ src/main/webapp/WEB-INF/views/Chat/chat_page.jsp
@@ -1,12 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<%@ taglib prefix="sec" uri= "http://www.springframework.org/security/tags"%> 
+
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Chat Page</title>
 	<c:import url="/WEB-INF/views/include/head.jsp"/>
+	<script src="https://cdn.jsdelivr.net/npm/sockjs-client/dist/sockjs.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/stompjs/lib/stomp.min.js"></script>
 </head>
 
 <c:import url="/WEB-INF/views/include/body_wrapper_start.jsp"/> 
@@ -19,6 +23,19 @@
 	
 		<c:import url="/WEB-INF/views/include/content_wrapper_start.jsp"/>
 			<!-- 내용 시작 -->
+			
+			
+			<sec:authentication property="principal.username" var="user"/>
+			<script type="text/javascript">
+				const user= "${user}"
+			</script>
+			<input type="text" id="messageInput">
+			<button id ="sendBtn">전송</button>
+			<br>
+			
+			<div id= "reciveMsg">
+			
+			</div>
 			<!-- 내용 끝 -->
 		<c:import url="/WEB-INF/views/include/content_wrapper_end.jsp"/>
 	
