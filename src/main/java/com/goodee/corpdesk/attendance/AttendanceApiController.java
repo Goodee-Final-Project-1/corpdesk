c3b7b5c8d4de603dce072fb9984c8f6259e2410b
DOO1212 <ejejgus@naver.com>
2025-09-16 15:32:01 +0900

    Revert "Fix: 버그 수정"
    This reverts commit 6d8a594db98f409790d3d4f20a62a589e42943b4.

--- src/main/java/com/goodee/corpdesk/attendance/AttendanceApiController.java
+++ src/main/java/com/goodee/corpdesk/attendance/AttendanceApiController.java
@@ -1,24 +1,32 @@
 package com.goodee.corpdesk.attendance;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class AttendanceApiController {
 	
-	@Autowired
-	private AttendanceService attendanceService;
+	private final AttendanceService attendanceService;
 	
-//	@GetMapping("attendances")
-//	public List<CalendarEventDTO> list(
-//	    @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-//	    @RequestParam("end")   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end,
-//	    @RequestParam(value="employeeId", required=false) Integer employeeId
-//	) {
-//	    LocalDateTime s = start.atStartOfDay();
-//	    LocalDateTime e = end.atTime(23, 59, 59);
-//	    return attendanceService.getEvents(s, e, employeeId);
-//	}
+	@GetMapping("attendances")
+	public List<CalendarEventDTO> list(
+	    @RequestParam("start") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
+	    @RequestParam("end")   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end,
+	    @RequestParam(value="employeeId", required=false) Integer employeeId
+	) {
+	    LocalDateTime s = start.atStartOfDay();
+	    LocalDateTime e = end.atTime(23, 59, 59);
+	    return attendanceService.getEvents(s, e, employeeId);
+	}
 
 	
 
