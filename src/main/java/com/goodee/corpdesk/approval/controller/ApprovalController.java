4f0724256a34871c1f7d25c3a2c34569472fb4fa
passing7by <jinam0717@gmail.com>
2025-09-17 11:42:06 +0900

    Update: 결재 요청, 취소, 처리 기능에 조건에 따른 기능 분기 추가
    # 15, #16

--- src/main/java/com/goodee/corpdesk/approval/controller/ApprovalController.java
+++ src/main/java/com/goodee/corpdesk/approval/controller/ApprovalController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.goodee.corpdesk.approval.dto.ApprovalDTO;
 import com.goodee.corpdesk.approval.dto.ApproverDTO;
+import com.goodee.corpdesk.approval.dto.RequestApprovalDTO;
 import com.goodee.corpdesk.approval.service.ApprovalService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -31,13 +32,13 @@ public class ApprovalController {
 	
 	// 결재 요청 
 	@PostMapping("")
-	public String submit(@RequestBody ApprovalDTO approvalDTO, @RequestBody ArrayList<ApproverDTO> approverDTOList) throws Exception {
+	public String submit(RequestApprovalDTO requestApprovalDTO) throws Exception {
 		
 		System.err.println("submit()");
 		
 		// TODO 로그인 기능과 합칠 때, modifiedBy를 username으로 변경, username을 submit 메서드의 매개변수로 받아오도록 수정
 		String modifiedBy = "test";
-		approvalDTO = approvalService.createApproval(approvalDTO, approverDTOList, modifiedBy);
+		ApprovalDTO approvalDTO = approvalService.createApproval(requestApprovalDTO, modifiedBy);
 		log.info("{}", approvalDTO);
 		
 		return approvalDTO.toString();
@@ -50,7 +51,8 @@ public class ApprovalController {
 		
 		System.err.println("cancel()");
 		
-		Boolean result = approvalService.deleteApproval(approvalId);
+		String result = approvalService.deleteApproval(approvalId); // NOT_FOUND, DENIED, PROCESSED 중 하나의 값이 반환됨
+		// TODO result 반환값에 따라 화면 표출 처리
 		log.info("{}", result);
 		
 		return result.toString();
@@ -58,16 +60,33 @@ public class ApprovalController {
 	}
 	
 	// 결재 승인/반려
-	@PutMapping("{approvalId}")
-	public String process(@PathVariable("approvalId") Long approvalId, Long approverId, String approveYn) throws Exception {
+	// 1. approvalId, approverId를 사용해 결재와 결재자 데이터를 받아와 수정하는 방식의 메서드
+	@PutMapping("{approvalId}/{approverId}/{approveYn}")
+	public String process(@PathVariable("approvalId") Long approvalId, @PathVariable("approverId") Long approverId, @PathVariable("approveYn") String approveYn) throws Exception {
 		
 		System.err.println("process()");
 		
-		// TODO
-		Boolean result = approvalService.processApproval(approvalId, approverId, approveYn);
+		String result = approvalService.processApproval(approvalId, approverId, approveYn); // NOT_FOUND, PROCESSED 중 하나의 값이 반환됨
 		log.info("{}", result);
 		
 		return result.toString();
 	}
+	
+	// 2. approvalId, employeeId를 사용해 결재와 결재자 데이터를 받아와 수정하는 방식의 메서드
+	/*
+	@PutMapping("{approvalId}/{approveYn}")
+	public String process(@PathVariable("approvalId") Long approvalId, @PathVariable("approveYn") String approveYn) throws Exception {
+		
+		System.err.println("process()");
+		
+		// TODO
+		// TODO 로그인 기능과 합칠 때, employeeId를 username으로 변경, username을 process 메서드의 매개변수로 받아오도록 수정
+		String employeeId = "3";
+		Boolean result = approvalService.processApproval(approvalId, employeeId, approveYn);
+		log.info("{}", result);
+		
+		return result.toString();
+	}
+	*/
 	
 }
