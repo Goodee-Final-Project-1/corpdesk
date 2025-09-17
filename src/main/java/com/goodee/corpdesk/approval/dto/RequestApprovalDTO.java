dbee42f71db22cf46240116f8f62f01c215fa015
passing7by <jinam0717@gmail.com>
2025-09-17 11:40:53 +0900

    Update: '결재 요청' 요청이 들어올 때, controller가 사용할 requestDTO 추가
    스프링 컨트롤러에서 List를 매개변수로 전달받는 방법
    방법1: @RequestBody를 사용한 JSON List 바인딩 -> 우선은 jsp를 사용한 ssr 방식으로 구현해야 하기때문에 탈락
    방법2: @RequestParam을 사용해 쿼리스트링으로 받기 -> 우리는 쿼리스트링을 사용하지 않고 패스밸류를 사용할 것이기 때문에 탈락
    방법2: List를 포함하는 DTO를 매개변수로 사용 -> 우리 시스템의 조건과 코드 컨벤션에 맞으므로 채택
    
    ReqApp 따로 파서 거기에 결재자 리스트, 결재 필드들 넣어놓음
    결재 요청시 ReqApp 사용
    ReqApp 에서 toApprovalEntity() 호출
    
    #15, #16

--- src/main/java/com/goodee/corpdesk/approval/dto/RequestApprovalDTO.java
+++ src/main/java/com/goodee/corpdesk/approval/dto/RequestApprovalDTO.java
@@ -1,5 +1,7 @@
 package com.goodee.corpdesk.approval.dto;
 
+import java.util.ArrayList;
+
 import com.goodee.corpdesk.approval.entity.Approval;
 
 import lombok.Getter;
@@ -9,16 +11,18 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class ApprovalDTO {
-	
+public class RequestApprovalDTO {
+
 	private Long approvalId;
 	private Integer employeeId;
 	private Integer departmentId;
 	private String formType;
 	private String formContent;
 	private Character status;
-		
-	public Approval toEntity() {
+	
+	ArrayList<ApproverDTO> approverDTOList;
+	
+	public Approval toApprovalEntity() {
 		return Approval.builder()
 					.approvalId(approvalId)
 					.employeeId(employeeId)
