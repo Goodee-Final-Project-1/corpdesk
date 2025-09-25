a1e85fa52a72b4176693f862a5ff4add989834f7
passing7by <jinam0717@gmail.com>
2025-09-25 18:45:36 +0900

    Update: HolidayController url 매핑 수정
    #73

--- src/main/java/com/goodee/corpdesk/holiday/controller/HolidayController.java
+++ src/main/java/com/goodee/corpdesk/holiday/controller/HolidayController.java
@@ -2,8 +2,10 @@ package com.goodee.corpdesk.holiday.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("holiday/**")
+@Controller
+@RequestMapping("/holiday/**")
 public class HolidayController {
 
 //    @GetMapping("test")
