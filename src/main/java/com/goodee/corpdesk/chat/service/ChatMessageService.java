11addd3ad6e434efed135c4c6e115e420670f408
huni99 <wjdrlfgns1@naver.com>
2025-09-30 22:16:27 +0900

    채팅방 상대 이미지 수정
    WIP : 채팅방의 상대이미지가 없을 때 기본 프로필로 안나오는 버그 수정
    #29

--- src/main/java/com/goodee/corpdesk/chat/service/ChatMessageService.java
+++ src/main/java/com/goodee/corpdesk/chat/service/ChatMessageService.java
@@ -26,93 +26,97 @@ public class ChatMessageService {
 
 	@Autowired
 	private ChatMessageRepository chatMessageRepository;
-	
+
 	@Autowired
 	private ChatParticipantRepository chatParticipantRepository;
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	//유저이름으로 유저 이름 부서 직위 가져옴
-		public String getUserNameDepPos(String username) {
-			Map<String, Object> map = employeeService.detail(username);
-	        Employee emp = (Employee) map.get("employee");
-	        Department department = (Department) map.get("department");
-	        Position position = (Position) map.get("position");
-	        String userNameDepPos = department.getDepartmentName() + " " + position.getPositionName() + " " + emp.getName();
-	        return userNameDepPos;
-			
+
+	// 유저이름으로 유저 이름 부서 직위 가져옴
+	public String getUserNameDepPos(String username) {
+		Map<String, Object> map = employeeService.detail(username);
+		Employee emp = (Employee) map.get("employee");
+		Department department = (Department) map.get("department");
+		Position position = (Position) map.get("position");
+		String userNameDepPos = department.getDepartmentName() + " " + position.getPositionName() + " " + emp.getName();
+		return userNameDepPos;
+
+	}
+
+	public String getUserImgPath(String username) {
+		Optional<EmployeeFile> empFileOp = employeeService.getEmployeeFileByUsername(username);
+		if (empFileOp.isPresent()) {
+			EmployeeFile empFile = empFileOp.get();
+			if (empFile != null && empFile.getUseYn()) {
+				return "/files/profile/" + empFile.getSaveName() + "." + empFile.getExtension();
+			} else {
+				return "/images/default_profile.jpg";
+			}
+		} else {
+			return "/images/default_profile.jpg";
 		}
-	
-	
+	}
+
 	public ChatMessage messageSave(ChatMessage msg) {
 		msg.setUseYn(true);
 		return chatMessageRepository.save(msg);
-		
+
 	}
-	
-	public List<ChatParticipant> participantListByRoom(Long RoomId){
+
+	public List<ChatParticipant> participantListByRoom(Long RoomId) {
 		return chatParticipantRepository.findAllByChatRoomIdAndUseYnTrue(RoomId);
 	}
-	public List<ChatParticipant> participantOnetoOneByRoom(Long RoomId){
+
+	public List<ChatParticipant> participantOnetoOneByRoom(Long RoomId) {
 		List<ChatParticipant> list = chatParticipantRepository.findAllByChatRoomId(RoomId);
-		
-		list.forEach(l->{
-			if(!l.getUseYn()) {
+
+		list.forEach(l -> {
+			if (!l.getUseYn()) {
 				chatParticipantRepository.updateRoomUseYnTrue(l.getEmployeeUsername(), RoomId);
 			}
 		});
 		return list;
 	}
 
-	//방번호로 해당 방의 메세지를 조회해옴
-	public List<ChatMessage> chatMessageList(Long chatRoomId, Long lastMessageNo, int size , Principal principal) {
+	// 방번호로 해당 방의 메세지를 조회해옴
+	public List<ChatMessage> chatMessageList(Long chatRoomId, Long lastMessageNo, int size, Principal principal) {
 		Pageable pageable = PageRequest.of(0, size);
-		
-		//방에 초대 되었을 시점을 기준까지 가져옴 (방을 나갔다 다시 들어온 경우 이전 내용을 볼 수 없음)
-		LocalDateTime enterTime = chatParticipantRepository.findByChatRoomIdAndEmployeeUsername(chatRoomId, principal.getName()).getUpdatedAt();
-		List<ChatMessage> list=null;
-		if(lastMessageNo==0|| lastMessageNo==null) {
-			 list =  chatMessageRepository.findByChatRoomIdAndSentAtGreaterThanEqualOrderByMessageIdDesc(chatRoomId,enterTime , pageable);
-			list.forEach(l->{
+
+		// 방에 초대 되었을 시점을 기준까지 가져옴 (방을 나갔다 다시 들어온 경우 이전 내용을 볼 수 없음)
+		LocalDateTime enterTime = chatParticipantRepository
+				.findByChatRoomIdAndEmployeeUsername(chatRoomId, principal.getName()).getUpdatedAt();
+		List<ChatMessage> list = null;
+		if (lastMessageNo == 0 || lastMessageNo == null) {
+			list = chatMessageRepository.findByChatRoomIdAndSentAtGreaterThanEqualOrderByMessageIdDesc(chatRoomId,
+					enterTime, pageable);
+			list.forEach(l -> {
 				l.setViewName(getUserNameDepPos(l.getEmployeeUsername()));
-				  Optional <EmployeeFile> empFileOp= employeeService.getEmployeeFileByUsername(l.getEmployeeUsername());
-			        if(empFileOp.isPresent()) {
-			        	EmployeeFile empFile = empFileOp.get();
-			        	 if(empFile!=null && empFile.getUseYn()) {
-			 	        	l.setImgPath("/files/profile/"+empFile.getSaveName()+"."+empFile.getExtension());
-			 	        }else {
-			 	        	l.setImgPath("/images/default_profile.jpg");
-			 	        }
-			        }
+				l.setImgPath(getUserImgPath(l.getEmployeeUsername()));
 				
+
 			});
-			
-		}else {
-			 list =chatMessageRepository.findByChatRoomIdAndMessageIdLessThanAndSentAtGreaterThanEqualOrderByMessageIdDesc(chatRoomId,lastMessageNo, enterTime, pageable);
-			list.forEach(l->{
+
+		} else {
+			list = chatMessageRepository
+					.findByChatRoomIdAndMessageIdLessThanAndSentAtGreaterThanEqualOrderByMessageIdDesc(chatRoomId,
+							lastMessageNo, enterTime, pageable);
+			list.forEach(l -> {
 				l.setViewName(getUserNameDepPos(l.getEmployeeUsername()));
-				 Optional <EmployeeFile> empFileOp= employeeService.getEmployeeFileByUsername(l.getEmployeeUsername());
-			        if(empFileOp.isPresent()) {
-			        	EmployeeFile empFile = empFileOp.get();
-			        	 if(empFile!=null && empFile.getUseYn()) {
-			 	        	l.setImgPath("/files/profile/"+empFile.getSaveName()+"."+empFile.getExtension());
-			 	        }else {
-			 	        	l.setImgPath("/images/default_profile.jpg");
-			 	        }
-			        }
+				l.setImgPath(getUserImgPath(l.getEmployeeUsername()));
 			});
 		}
 		return list;
 	}
 
 	public Long checkFirstMessage(Long chatRoomId) {
-			return chatMessageRepository.countByChatRoomId(chatRoomId);
-		
+		return chatMessageRepository.countByChatRoomId(chatRoomId);
+
 	}
 
 	public void RoomUserAllUseYnTrue(Long chatRoomId) {
 		chatParticipantRepository.updateAllRoomUseYnTrue(chatRoomId);
-		
+
 	}
 
 }
